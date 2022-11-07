b840f026dcf086e66d534e19ea1f4cf487cb4914
kankordkoks <konstantinkankord@gmail.com>
2022-11-07 20:06:41 +0300

    feat: modified js04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,29 +1,19 @@
-
-const { readdir, copyFile, mkdir, rm } = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
-const { stdout } = process;
 
-const source = path.join(__dirname, 'files');
-const purpose = path.join(__dirname, 'copy');
 
-async function copyDir(from, to) {
-	try {
-		const dir = await readdir(from, { withFileTypes: true });
-		for (const file of dir) {
-			if (file.isFile()) {
-				await copyFile(path.join(from, file.name), path.join(to, file.name));
-			} else if (file.isDirectory()) {
-				await mkdir(path.join(to, file.name));
-				await copyDir(path.join(from, file.name), path.join(to, file.name));
-			}
-		}
-	} catch (error) {
-		stdout.write(error.message);
-	}
-}
+fs.mkdir(__dirname + '/files-copy', { recursive: true }, err => {
+	if (err) throw err;
+});
+const copyPath = path.join(__dirname, 'files-copy');
+const oldPath = path.join(__dirname, 'files');
 
-(async function () {
-	await rm(purpose, { recursive: true, force: true });
-	await mkdir(purpose, { recursive: true });
-	await copyDir(source, purpose);
-})();
+fs.readdir(oldPath, (err, data) => {
+	if (err) throw err;
+	data.forEach(file => {
+		fs.copyFile(oldPath + '\\' + file, copyPath + '\\' + file, err => {
+			if (err) throw err;
+			console.log('File was copied');
+		});
+	});
+});
